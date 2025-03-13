9b6ed371605413aeed13b912e2c7b1603d3a6182
dharnidhar <example@gmail.com>
2025-03-13 15:39:59 +0530

    moving file to git

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const studentRoutes = require("./routes/studentRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/studentsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/students", studentRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
